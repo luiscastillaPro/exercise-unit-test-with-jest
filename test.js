4bcc54862e8a70528aeecab49f61298488240bbe
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-08-02 14:59:11 +0000

    First

--- test.js
+++ test.js
@@ -12,6 +12,8 @@ test("Conversion from USD to JPY (1 USD = 149.03 JPY)", function() {
     expect(total).toBe(1490.3);
 })
 
+
+
 const { fromYenToPound } = require('./app.js')
 
 test("Conversion from JPY to GBP (1 JPY = 0.0072 GBP)", function() {
